cbd38ddcf4d13a1d28668c7e4f950700ceed1976
dharundev81 <dharundev81@gmail.com>
2017-09-19 01:21:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,13 +14,46 @@ var articleOne = {
         <pre>ARTICLE ONE</pre>`
 };
 
+function createTemplate (data){
+    
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+
+var htmlTemplate = `
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    </head>
+    <body>
+        <h1>
+            ${heading}
+        </h1>
+        <div>
+            ${date}
+        </div>
+        <div>
+            ${content}
+        </div>
+    </body>
+</html>
+
+`;
+
+return htmlTemplate;
+
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/demo', function(req,res){
     //res.send("THIS IS Dharun Babu");
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    //res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/ui/style.css', function (req, res) {
